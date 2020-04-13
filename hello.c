1390fe9eab23a5862045e300b9b0991c9bb68df5
szbitzel <alexionutcraciun@gmail.com>
2020-04-13 06:54:41 -0700

    Repo modificat Hello name!

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-int main()
+int main (int argc,char ∗argv[])
 {
-      printf("Hello world!\n");
-      
-      return 0;
-}      
+if (argc!= 2) {
+printf ("USAGE: %s name\n " ,argv[0]) ;
+return −1;
+}
+printf("Hello, %s!\n",argv[1]);
+return 0;
+}
+
